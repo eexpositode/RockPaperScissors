aad7440d97d250081971a14657cc75fcd9d2f557
EduardoExposito <eduardo.exposito@talea.de>
2020-06-17 10:52:21 +0100

    Build mapActionsToResult(): test method return sth

--- src/RockPaperScissor.kt
+++ src/RockPaperScissor.kt
@@ -1,8 +1,9 @@
+import kotlin.random.Random
+
 fun main() {
     print("Hello World")
 }
 
 //TODO playRockPaperScissor() : Void
-//TODO mapActionsToResult(actionPlayer1: Int, actionPlayer2: Int)
-//TODO getRandomActions(n: Int) : List<Int>
-fun getRandomActions() = listOf(1)
+//TODO mapActionsToResult(actionPlayer1: Int, actionPlayer2: Int) : Int
+fun getRandomActions(gameRounds: Int) = List(gameRounds){ Random.nextInt(3) }
--- tests/RockPaperScissorKtTest.kt
+++ tests/RockPaperScissorKtTest.kt
@@ -1,26 +1,44 @@
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Nested
 
-internal class RockPaperScissorKtTest {
+class RockPaperScissorKtTest {
 
-    @Test
-    fun `test getRandomActions has a return value`() {
-        assertNotNull(getRandomActions(10))
+    @Nested
+    inner class TestGetRandomActions {
+        @Test
+        fun `test getRandomActions has a return value`() {
+            assertNotNull(getRandomActions(10))
+        }
+
+        @Test
+        fun `test getRandomActions returns a list of integers`() {
+            assertTrue(getRandomActions(10) is List<Int>)
+        }
+
+        @Test
+        fun `test getRandomActions returns a non-empty list`() {
+            assertTrue(getRandomActions(10).isNotEmpty())
+        }
+
+        @Test
+        fun `test getRandomActions returns a list with size n`() {
+            assertTrue(getRandomActions(10).size == 10)
+        }
+
+        @Test
+        fun `test getRandomActions returns a random list with values 0 1 2`() {
+            assertTrue(getRandomActions(10).all { it in 0..3 })
+        }
     }
 
-    @Test
-    fun `test getRandomActions returns a list of integers`() {
-        assertTrue(getRandomActions(10) is List<Int>)
-    }
+    @Nested
+    inner class TestMapActionsToResult {
 
-    @Test
-    fun `test getRandomActions returns a non-empty list`() {
-        assertTrue(getRandomActions(10).isNotEmpty())
-    }
-
-    @Test
-    fun `test getRandomActions returns a list with size n`() {
-        assertTrue(getRandomActions(10).size == 10)
+        @Test
+        fun `test mapActionsToResult has a return value`() {
+            assertNotNull(mapActionsToResult())
+        }
     }
 }
