22d3a5647eb569e0cf4dba9999b892419d9a7668
EduardoExposito <eduardo.exposito@talea.de>
2020-06-17 15:44:57 +0100

    Call methods from the main() function and do some refactoring

--- src/RockPaperScissor.kt
+++ src/RockPaperScissor.kt
@@ -2,27 +2,7 @@ import java.lang.RuntimeException
 import kotlin.random.Random
 
 fun main() {
-}
-
-fun resultsToStdout(results: List<Int>) {
-    println("Player 1 no. of wins: 0")
-    println("Player 1 no. of draws: ${results.size}")
-    println("Player 2 no. of wins: 0")
-    print("Player 2 no. of draws: ${results.size}")
-}
-
-/**
- * An input action is mapped to a game result. The assumption is that one player always plays
- * rock while the other one plays randomly.
- * @param action Integer representing an action Stone - 0, Paper - 1, Scissors - 2
- * @return A game result depending on the input action: Draw - 0, Win - 1, Loss - 2
- * @throws RuntimeException if input action not in range 0 .. 3
- */
-fun mapActionsToResult(action: Int) = when (action) {
-    0 -> 0
-    1 -> 1
-    2 -> 2
-    else -> throw RuntimeException("Unexpected input action")
+    getRandomActions(100).mapActionsToResults().resultsToStdout()
 }
 
 /**
@@ -31,3 +11,31 @@ fun mapActionsToResult(action: Int) = when (action) {
  * @return Random List<Int> with size gameRounds
  */
 fun getRandomActions(roundsCount: Int) = List(roundsCount) { Random.nextInt(3) }
+
+/**
+ * Extension function that maps an action to a game result. The assumption is that one player always plays
+ * rock while the other one plays randomly.
+ * Actions: Stone - 0, Paper - 1, Scissors - 2
+ * @return A game result depending on the input action: Draw - 0, Win - 1, Loss - 2
+ * @throws RuntimeException if input action not in range 0 .. 3
+ */
+fun List<Int>.mapActionsToResults() = map { action ->
+    when (action) {
+        0 -> 0
+        1 -> 1
+        2 -> 2
+        else -> throw RuntimeException("Unexpected input action")
+    }
+}
+
+/**
+ * Maps a list of results to the desired output format
+ * - Player 1 Wins / Draws
+ * - Player 2 Wins / Draws
+ */
+fun List<Int>.resultsToStdout() {
+    println("Player 1 no. of wins: ${count { it == 1 }}")
+    println("Player 1 no. of draws: ${count { it == 0 }}")
+    println("Player 2 no. of wins: ${count { it == 2 }}")
+    print("Player 2 no. of draws: ${count { it == 0 }}")
+}
--- tests/RockPaperScissorKtTest.kt
+++ tests/RockPaperScissorKtTest.kt
@@ -1,11 +1,11 @@
 import org.junit.jupiter.api.*
-
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.CsvSource
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
-import java.lang.RuntimeException
+import java.util.stream.Stream
 
 internal class RockPaperScissorKtTest {
 
@@ -42,23 +42,22 @@ internal class RockPaperScissorKtTest {
 
         @Test
         fun `test mapActionsToResult has a return value`() {
-            assertNotNull(mapActionsToResult(0))
+            assertNotNull(emptyList<Int>().mapActionsToResults())
         }
 
         @Test
-        fun `test mapActionsToResult returns an integer`() {
-            assertTrue(mapActionsToResult(0) is Int)
+        fun `test mapActionsToResult returns a list of integers`() {
+            assertTrue(emptyList<Int>().mapActionsToResults() is List<Int>)
         }
 
-        @ParameterizedTest
-        @CsvSource("0, 0", "1, 1", "2, 2")
-        fun `test mapActionsResult results`(input: Int, result: Int) {
-            assertEquals(result, mapActionsToResult(input))
+        @Test
+        fun `test mapActionsResult results`() {
+            assertEquals(arrayListOf(0, 1, 2), arrayListOf(0, 1, 2).mapActionsToResults())
         }
 
         @Test
         fun `test mapActionsResult throws an Exception on unknown input action`() {
-            assertThrows(RuntimeException::class.java) { mapActionsToResult(5) }
+            assertThrows(RuntimeException::class.java) { arrayListOf(5).mapActionsToResults() }
         }
     }
 
@@ -66,7 +65,7 @@ internal class RockPaperScissorKtTest {
     @Nested
     inner class TestResultsToStdout {
 
-        private val stdOutput : ByteArrayOutputStream by lazy { ByteArrayOutputStream() }
+        private val stdOutput: ByteArrayOutputStream by lazy { ByteArrayOutputStream() }
 
         @BeforeEach
         fun setUp() {
@@ -81,19 +80,19 @@ internal class RockPaperScissorKtTest {
 
         @Test
         fun `test resultsToStdout prints on the stout`() {
-            resultsToStdout(emptyList())
+            emptyList<Int>().resultsToStdout()
             assertTrue(stdOutput.toString().isNotBlank())
         }
 
         @Test
         fun `test resultsToStdout prints 4 lines`() {
-            resultsToStdout(emptyList())
+            emptyList<Int>().resultsToStdout()
             assertEquals(4, stdOutput.toString().split("\n").size)
         }
 
         @Test
         fun `test resultsToStdout prints wins and draws for each player`() {
-            resultsToStdout(emptyList())
+            emptyList<Int>().resultsToStdout()
             stdOutput.toString().split("\n").let {
                 assertTrue(it[0].startsWith("Player 1 no. of wins: "))
                 assertTrue(it[1].startsWith("Player 1 no. of draws: "))
@@ -102,14 +101,29 @@ internal class RockPaperScissorKtTest {
             }
         }
 
-        @Test
-        fun `test resultsToStdout when results are all draws`() {
-            resultsToStdout(List(10) {0})
+        @Suppress("unused")
+        private fun generateArgumentsStream() = Stream.of(
+                Arguments.of(List(10) { 0 }, 0, 10, 0, 10),
+                Arguments.of(List(10) { 1 }, 10, 0, 0, 0),
+                Arguments.of(List(10) { 2 }, 0, 0, 10, 0),
+                Arguments.of(arrayListOf(0, 1, 0, 2, 1, 1, 1, 2, 2, 0), 4, 3, 3, 3)
+        )
+
+        @ParameterizedTest
+        @MethodSource("generateArgumentsStream")
+        fun `test resultsToStdout when results are all draws`(
+                results: List<Int>,
+                expectedPlayerAWins: Int,
+                expectedPlayerADraws: Int,
+                expectedPlayerBWins: Int,
+                expectedPlayerBDraws: Int
+        ) {
+            results.resultsToStdout()
             stdOutput.toString().split("\n").let {
-                assertTrue(it[0].startsWith("Player 1 no. of wins: 0"))
-                assertTrue(it[1].startsWith("Player 1 no. of draws: 10"))
-                assertTrue(it[2].startsWith("Player 2 no. of wins: 0"))
-                assertTrue(it[3].startsWith("Player 2 no. of draws: 10"))
+                assertTrue(it[0].startsWith("Player 1 no. of wins: $expectedPlayerAWins"))
+                assertTrue(it[1].startsWith("Player 1 no. of draws: $expectedPlayerADraws"))
+                assertTrue(it[2].startsWith("Player 2 no. of wins: $expectedPlayerBWins"))
+                assertTrue(it[3].startsWith("Player 2 no. of draws: $expectedPlayerBDraws"))
             }
         }
     }
